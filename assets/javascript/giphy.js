283d09c44ed76008be5a9e086ed292d7aed92ec7
Roderick Trice <Rodericktrice@Rodericks-MacBook-Pro.local>
2018-07-28 13:49:48 -0400

    added comments to the addBtns function

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,18 +1,19 @@
-var topics = ["cats", "dogs", "afvbabies", "afvpets", "afv", "funnykids", "funny", "tackle", "afvepicfalls"];
+var topics = ["cats", "dogs", "afv babies", "afv pets", "afv", "funny kids", "funny", "tackle", "afv epic falls"];
 
 function addBtns() {
-
+//  function for adding the buttons dynamically
     $("#FunnyBtn").empty();
+    // looping through the array of topics
     for (var i = 0; i < topics.length; i++) {
-
+        // creating button for every string in the array
         var a = $("<button>");
-
+        // adding class of topics to the button
         $(a).addClass("topics");
-
+        // adding data attribute
         $(a).attr("data-name", topics[i]);
-
+        // this is providing the initial button text
         $(a).text(topics[i]);
-
+        // finally we append the button to the  HTML
         $("#FunnyBtn").append(a);
 
 
