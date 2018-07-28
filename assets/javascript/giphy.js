93b9877234fe0a7b846469e011d4431b4264b876
Roderick Trice <Rodericktrice@Rodericks-MacBook-Pro.local>
2018-07-28 19:32:56 -0400

    starting over. didnt like the direction i was going in

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,61 +1,84 @@
-var topics = ["cats", "dogs", "afv babies", "afv pets", "afv", "funny kids", "funny", "tackle", "afv epic falls"];
+var topics = ["basketball", "football", "soccer", "tennis", "hockey", "baseball"];
 
-    //  function for adding the buttons dynamically
+//  function for adding the buttons dynamically
 function addBtns() {
     $("#laughterButtons").empty();
     // looping through the array of topics
     for (var i = 0; i < topics.length; i++) {
         // creating button for every string in the array
-        var funnyBtn = $("<button>");
+        var sportBtn = $("<button>");
         // adding class of topics to the button
-        $(funnyBtn).addClass("topics");
+        $(sportBtn).addClass("topics");
         // adding data attribute
-        $(funnyBtn).attr("data-name", topics[i]);
+        $(sportBtn).attr("data-name", topics[i]);
         // this is providing the initial button text
-        $(funnyBtn).text(topics[i]);
+        $(sportBtn).text(topics[i]);
         // finally we append the button to the  HTML
-        $("#laughterButtons").append(funnyBtn);
+        $("#sportButtons").append(sportBtn);
     }
-} 
+}
 // this displays the initial buttons
 addBtns();
-    // when the button is clicked it handles the evens
-    $("#add-topic").on("click", function (event) {
-        // prevents the buttons from submitting a form(its default behavior)
-        event.preventDefault();
-        // this is where the function grabs the input from the textbox
-        var addTopic = $("#jokester-input").val();
-        // this adds a topic to the array
-        topics.push(addTopic);
+// when the button is clicked it handles the evens
+$("#add-topic").on("click", function (event) {
+    // prevents the buttons from submitting a form(its default behavior)
+    event.preventDefault();
+    // this is where the function grabs the input from the textbox
+    var addTopic = $("sports-inputs").val();
+    // this adds a topic to the array
+    topics.push(addTopic);
 
-        console.log(topics);
-        // calling the addBtns/processes the topics array
-        addBtns();
-    })
-
-
-
-// $(document).on("click", function () {
-    // var name = $(this).attr("data-name");
-
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=8KlorxDnGq90aNmGgX8hb5tF5c5UGvig&limit=10";
-    $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .then(function (response) {
-            
-            console.log(response);
+    console.log(topics);
+    // calling the addBtns/processes the topics array
+    addBtns();
+})
 
 
 
+$(document).on("click", function () {
+            var sport = $(this).attr("data-sports");
 
-        });
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=8KlorxDnGq90aNmGgX8hb5tF5c5UGvig&limit=10";
+            $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                })
+                .then(function (response) {
+
+                        console.log(response);
+
+                        var results = response.data;
+
+                        for (var i = 0; i < results.length; i++) {
+
+                            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                                // Creating a div with the class "item"
+                                var gifDiv = $("<div class='item'>");
+
+                                // Storing the result item's rating
+                                var rating = results[i].rating;
+
+                                // Creating a paragraph tag with the result item's rating
+                                var p = $("<p>").text("Rating: " + rating);
+
+                                // Creating an image tag
+                                var gifImage = $("<img>");
+
+                                // Giving the image tag an src attribute of a proprty pulled off the
+                                // result item
+                                gifImage.attr("src", results[i].images.fixed_height.url);
+
+                                // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                                gifDiv.append(p);
+                                gifDiv.append(gifImage);
+
+                                // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                                $("#sports-box").prepend(gifDiv);
+                            }
 
 
 
 
-// })
-
-
-
+                        };
+                    });
+                });
