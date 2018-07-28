b4ca8739a7b95e2e4f7c799828ae0b6934923d8f
Roderick Trice <Rodericktrice@Rodericks-MacBook-Pro.local>
2018-07-28 14:50:16 -0400

    was able to add a topic from the sumbit button

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,8 +1,8 @@
 var topics = ["cats", "dogs", "afv babies", "afv pets", "afv", "funny kids", "funny", "tackle", "afv epic falls"];
 
 function addBtns() {
-//  function for adding the buttons dynamically
-    $("#FunnyBtn").empty();
+    //  function for adding the buttons dynamically
+    $("#buttons").empty();
     // looping through the array of topics
     for (var i = 0; i < topics.length; i++) {
         // creating button for every string in the array
@@ -14,11 +14,25 @@ function addBtns() {
         // this is providing the initial button text
         $(a).text(topics[i]);
         // finally we append the button to the  HTML
-        $("#FunnyBtn").append(a);
+        $("#buttons").append(a);
 
 
     }
 
 
 }
+// when the button is clicked it handles the evens
+$("#add-topic").on("click", function(event) {
+    // prevents the buttons from submitting a form(its default behavior)
+    event.preventDefault();
+    // this is where the function grabs the input from the textbox
+    var addTopic = $("#jokester-input").val();
+// this adds a topic to the array
+    topics.push(addTopic);
+
+    console.log(topics);
+    // calling the addBtns/processes the topics array
+    addBtns();
+})
+// this displays the initial buttons
 addBtns();
